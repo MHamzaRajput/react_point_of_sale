d6cce499def34207c4b4541ff957a0690f4b1118
Hamza <Hamza@advoss.com>
2019-10-08 22:54:30 +0500

    update code remove warnings

---
+++ react-pos/src/js/components/Checkout.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import { Modal, Button } from "react-bootstrap";
+
+class Checkout extends Component {
+  render() {
+    const { total, modal } = this.props;
+    return (
+      <div>
+        <button
+          className="btn btn-success lead"
+          id="checkoutButton"
+          ng-click="focusPayment()"
+          ng-disabled="cartTotal === 0"
+          data-toggle="modal"
+          data-target="#checkoutModal"
+        >
+          <i className="glyphicon glyphicon-shopping-cart" />
+          <br />
+          <br />
+          C<br />
+          h<br />
+          e<br />
+          c<br />
+          k<br />
+          o<br />
+          u<br />t
+        </button>
+        <div className="modal-body">
+          <Modal show={total}>
+            <Modal.Header closeButton>
+              <Modal.Title>Checkout</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div ng-hide="transactionComplete" className="lead">
+                <h3>
+                  Total: <span className="text-danger"> {total} </span>
+                </h3>
+                <h3>
+                  Change Due:
+                  <span className="text-success">
+                    getChangeDue() | currency{" "}
+                  </span>
+                </h3>
+
+                <form className="form-horizontal" name="checkoutForm">
+                  <div
+                    className="form-group"
+                    ng-class="{ 'has-error': checkoutForm.payment.$error.required }"
+                  >
+                    <div className="input-group">
+                      <div className="input-group-addon">$</div>
+                      <input
+                        type="number"
+                        id="checkoutPaymentAmount"
+                        className="form-control input-lg"
+                        name="payment"
+                        step="any"
+                        min="0"
+                        ng-model="paymentAmount"
+                        required
+                      />
+                    </div>
+                  </div>
+
+                  <p className="text-danger">Enter payment amount.</p>
+
+                  <button type="submit" className="btn btn-primary btn-lg lead">
+                    Print Receipt
+                  </button>
+                </form>
+              </div>
+
+              <div
+                ng-show="transactionComplete"
+                class="lead"
+                ng-if="checkoutForm.payment"
+              >
+                <h3>
+                  Total:
+                  <span className="text-danger">
+                    previousCartInfo.total | currency
+                  </span>
+                </h3>
+                <h3>
+                  Change Due:
+                  <span className="text-success">
+                    previousCartInfo.paymentAmount - previousCartInfo.total |
+                    currency
+                  </span>
+                </h3>
+                <button
+                  ng-click="closeModal()"
+                  className="btn btn-default btn-lg"
+                  type="button"
+                >
+                  Close
+                </button>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleSubmit}>Add</Button>
+              <Button onClick={() => this.setState({ showModal: false })}>
+                Cancel
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Checkout;
---
+++ react-pos/src/js/components/Transactions.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./Header";
+import CompleteTransactions from "./CompleteTransactions";
+import axios from "axios";
+import moment from "moment";
+
+const HOST = "http://localhost:8001";
+const url = HOST + `/api/all`;
+
+class Transactions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { transactions: [] };
+  }
+  componentWillMount() {
+    axios
+      .get(url)
+      .then(response => this.setState({ transactions: response.data }))
+      .catch(err => {
+        console.log(err);
+      });
+  }
+  render() {
+    var { transactions } = this.state;
+
+    var rendertransactions = () => {
+      if (transactions.length === 0) {
+        return (
+          <tr key="0">
+            <th>No Transactions found</th>
+          </tr>
+        );
+      } else {
+        return transactions.map(transaction => (
+          <CompleteTransactions key={transaction._id} {...transaction} />
+        ));
+      }
+    };
+
+    return (
+      <div>
+        <Header />
+        <br />
+        <br />
+
+        <table className="table table-hover table-striped">
+          <thead>
+            <tr>
+              <th>Time</th>
+              <th>Total</th>
+              <th>Products</th>
+              <th>Open</th>
+            </tr>
+          </thead>
+          <tbody>{rendertransactions()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Transactions;
